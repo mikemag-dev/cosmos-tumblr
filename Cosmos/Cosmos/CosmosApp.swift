de4955a290a6ed7fa4d6e0c1bbd34edbfee89778
mike <3445394+mikemag-dev@users.noreply.github.com>
2025-04-10 16:57:08 -0400

    initial commit

---
+++ Cosmos/Cosmos/CosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosApp.swift
+//  Cosmos
+//
+//  Created by mike on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cosmos/CosmosTests/CosmosTests.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosTests.swift
+//  CosmosTests
+//
+//  Created by mike on 4/10/25.
+//
+
+import Testing
+@testable import Cosmos
+
+struct CosmosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
